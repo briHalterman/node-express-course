a04de7eda11525880b78b3b485f8945c1b02926c
Brittany Halterman <halterma@gmail.com>
2023-06-12 22:37:34 -0400

    Add tutorial code to intro.js

--- 01-node-tutorial/answers/intro.js
+++ 01-node-tutorial/answers/intro.js
@@ -1,3 +1,23 @@
-const amount = Math.floor(Math.random() * 101);
+// Intro
 
-console.log(`Hello Node! Congratulations! Your number is ${amount}...`);
+// This program should use console.log to write something to the screen. 
+// While you are in the answers directory, run node intro to verify that the program runs. 
+// You can also put JavaScript logic in your program.
+
+// Tutorial code
+
+const amount = 1;
+
+if(amount < 10) {
+    console.log('small number')
+}
+else {
+    console.log('large number')
+};
+
+console.log('Hello Node')
+
+// const amount = Math.floor(Math.random() * 101);
+
+// console.log(`Hello Node! Congratulations! Your number is ${amount}...`);
+
