cff8b85528673120252bb45faf4eccfd4873e941
Brittany Halterman <halterma@gmail.com>
2023-06-13 07:09:58 -0400

    Created names program based on tutorial

---
+++ 01-node-tutorial/answers/04-names.js
@@ -0,0 +1,16 @@
+// Names
+
+// Should export multiple values in an object
+// The exported values from should be used in modules.js
+
+// Tutorial code
+
+// local
+const secret = 'SOOPER SECRET';
+
+// share
+const scott = 'Scott';
+const leo = 'Leo';
+
+console.log( scott, leo );
+
