57d3222cea78175fb54a7d3034146825d34e032f
Brittany Halterman <halterma@gmail.com>
2023-07-04 18:55:11 -0400

    improve number guessing game

--- 01-node-tutorial/answers/prompter.js
+++ 01-node-tutorial/answers/prompter.js
@@ -21,19 +21,18 @@ const getBody = (req, callback) => {
 };
 
 // here, you could declare one or more variables to store what comes back from the form.
-const amount = Math.floor(Math.random() * 101);
-const answer = "";
+const secretNumber = Math.floor(Math.random() * 101);
 
-console.log(`Hello Node! Congratulations! Your number is ${amount}...`);
+console.log(`Hello Node! Congratulations! Your number is ${secretNumber}...`);
 
 // here, you can change the form below to modify the input fields and what is displayed.
 // This is just ordinary html with string interpolation.
-const form = (answer) => {
+const form = (text) => {
   return `
   <body>
   <h1>Guess a Number!</h1>
   <h2>Guess a number between 1 and 100</h2>
-  <p>${answer}</p>
+  <p>${text}</p>
   <p>Guess an integer in numeric form, then press 'Submit Guess'</p>
   <form method="POST">
   <input name="guess" type="number"></input>
@@ -50,16 +49,18 @@ const server = http.createServer((req, res) => {
     getBody(req, (body) => {
       console.log("The body of the post is ", body);
       // here, you can add your own logic
-      const guess = parseInt(body["guess"])
-      if (guess > amount) {
-        answer = "Your guess is too high";
-      } else if(guess < amount) {
-        answer = "Your guess is too low";
-      } else if(guess === amount) {
-        answer = "You win!"
+      const guessNumber = parseInt(body.guess)
+      let resultText = '';
+      if (guessNumber > secretNumber) {
+        resultText = "Your guess is too high";
+      } else if(guessNumber < secretNumber) {
+        resultText = "Your guess is too low";
+      } else if(guessNumber === secretNumber) {
+        resultText = "You win!"
       } else {
-        answer = "Trouble"
+        resultText = "Trouble"
       }
+
       // Your code changes would end here
       res.writeHead(303, {
         Location: "/",
@@ -67,7 +68,7 @@ const server = http.createServer((req, res) => {
       res.end();
     });
   } else {
-    res.end(form(answer));
+    res.end(form());
   }
 });
 
