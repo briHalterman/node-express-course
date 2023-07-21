0ff623b2bfdfbf991d9950ce4226c59a38cc9455
Brittany Halterman <halterma@gmail.com>
2023-07-21 09:19:52 -0400

    Create .gitignore, create package.json, add notes to intro.js

--- 01-node-tutorial/answers/01-intro.js
+++ 01-node-tutorial/answers/01-intro.js
@@ -4,7 +4,23 @@
 // While you are in the answers directory, run node intro to verify that the program runs
 // You can also put JavaScript logic in your program
 
-// Tutorial code
+// WHAT IS NODE.JS????
+// --Environment to run JS outside Browser
+// --Built on Chrome's V8 JS Engine
+// --Big Community
+// --Full-Stack
+
+// COURSE REQUIREMENTS
+// HTML, CSS, JS, ES6
+// Callbacks, Promises, Async-Await
+
+// NODE.JS
+// --No DOM
+// --No Window
+// Server Side Apps
+// --Filesystem
+// --Versions
+// --CommonJS
 
 const amount = 1;
 
