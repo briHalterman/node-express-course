89417aa40b42729ea17993138b726acfbbfaca1c
Brittany Halterman <halterma@gmail.com>
2023-09-04 11:37:18 -0400

    set up the rest of the controllers

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,11 +1,35 @@
-// create controller function
+// set up the rest of the controllers
 
 // Get All Tasks (GET)
-
 const getAllTasks = (req, res) => {
-    res.send('get all tasks');
+    res.send('all items from the file');
 };
 
+// Create Task (POST)
+const createTask = (req, res) => {
+    res.send('create task');
+}
+
+// Get Task (GET)
+const getTask = (req, res) => {
+    res.send('get single tasks');
+} ;
+
+// Delete Task (DELETE)
+const deleteTask = (req, res) => {
+    res.send('delete task');
+};
+
+// Update Task (POST)
+const updateTask = async (req, res) => {
+    res.send('update task');
+};
+
+
 module.exports = {
-    getAllTasks
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
 }
--- 03-task-manager/starter/routes/tasks.js
+++ 03-task-manager/starter/routes/tasks.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllTasks } = require('../controllers/tasks');
+const { 
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+} = require('../controllers/tasks');
 
 // router.route('/').get((req, res) => {
 //     res.send('all items')
 // });
-router.route('/').get(getAllTasks);
 
-module.exports = router
+// set up routes
+router.route('/').get(getAllTasks).post(createTask);
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask);
+
+module.exports = router;
