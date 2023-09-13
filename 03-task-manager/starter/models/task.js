c74e280e3d5fa41e202e91aff6e12a609174eb0f
Brittany Halterman <halterma@gmail.com>
2023-09-13 18:43:40 -0400

    add comments to schema

--- 03-task-manager/starter/models/task.js
+++ 03-task-manager/starter/models/task.js
@@ -5,16 +5,17 @@ const mongoose = require('mongoose');
 const TaskSchema = new mongoose.Schema({
     // name: String,
     name: {
-        type: String,
+        type: String, // set up type
         // required: true
-        required: [true, 'must provide name'],
-        trim: true,
-        maxlength: [20, 'name cannot be more than 20 characters']
-    }, 
+        required: [true, 'must provide name'], // required? true/false? custom message?
+        trim: true, // no whitespace
+        maxlength: [20, 'name cannot be more than 20 characters'] // min avail too
+    }, // set up property equal to an object
+    
     // completed: Boolean
     completed: {
         type: Boolean,
-        default: false
+        default: false // default value option
     }
 });
 
