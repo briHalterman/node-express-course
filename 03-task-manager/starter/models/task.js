bdbced20e72b7d5584076baf4ba07b2ad3ed0783
Brittany Halterman <halterma@gmail.com>
2023-09-13 18:32:50 -0400

    set up Mongoose schema

--- 03-task-manager/starter/models/task.js
+++ 03-task-manager/starter/models/task.js
@@ -3,8 +3,19 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-    name: String, 
-    completed: Boolean
+    // name: String,
+    name: {
+        type: String,
+        // required: true
+        required: [true, 'must provide name'],
+        trim: true,
+        maxlength: [20, 'name cannot be more than 20 characters']
+    }, 
+    // completed: Boolean
+    completed: {
+        type: Boolean,
+        default: false
+    }
 });
 
 // model as representation of collection
