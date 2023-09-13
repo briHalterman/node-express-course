17ec9e35b1064fe5713a44a45841a92564dbd742
Brittany Halterman <halterma@gmail.com>
2023-09-13 19:18:40 -0400

    set up controllers for getAllTasks

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,16 +1,29 @@
 // START USING MODEL
 const Task = require('../models/task')
+
 // an instance of a model is call a document
 // the first argument is the singular name of the collection the model is for
 
 // SET UP CONTROLLERS
 
 // Get All Tasks (GET)
-const getAllTasks = (req, res) => {
-    res.send('get all tasks');
+
+// const getAllTasks = (req, res) => {
+//     res.send('get all tasks');
+// };
+
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({});
+        res.status(200).json({ tasks }); // res.status(200).json({ tasks: tasks })
+
+    } catch (error) {
+        res.status(500).json({ msg: error });
+    };
 };
 
 // Create Task (POST)
+
 // const createTask = (req, res) => {
 //     // res.send('create task');
 //     res.json(req.body); // send what we are getting from the client
@@ -19,7 +32,6 @@ const getAllTasks = (req, res) => {
 // const createTask = async (req, res) => {
 //     // const task = await Task.create({ name: 'first task' }) // hardcoded
 //     // res.json(req.body);
-
 //     const task = await Task.create(req.body);
 //     res.status(201).json({ task });
 // }
