c9b377f4a3c9e47406e71082262a91dfb5d84ee3
Brittany Halterman <halterma@gmail.com>
2023-09-16 17:12:39 -0400

    refractor deleteTask route

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -58,7 +58,7 @@ const asyncWrapper = require('../middleware/async');
 // });
 
 // remove try/catch block completely
-const getAllTasks = asyncWrapper (async (req, res) => {
+const getAllTasks = asyncWrapper(async (req, res) => {
     const tasks = await Task.find({});
     res.status(200).json({ tasks });
 });
@@ -131,20 +131,30 @@ const getTask = asyncWrapper(async (req, res) => {
 //     res.send('delete task');
 // };
 
-const deleteTask = async (req, res) => {
-    try {
-        const { id: taskID } = req.params;
-        const task = await Task.findOneAndDelete({ _id: taskID });
-        if (!task) {
-            return res.status(404).json({ msg: `No task with id : ${taskID}` });
-        };
-        res.status(200).json({ task });
-        // res.status(200).send();
-        // res.status(200).json({ task: null, status: 'success' });
-    } catch (error) {
-        res.status(500).json({ msg: error })
+// refractor route
+// const deleteTask = async (req, res) => {
+//     try {
+//         const { id: taskID } = req.params;
+//         const task = await Task.findOneAndDelete({ _id: taskID });
+//         if (!task) {
+//             return res.status(404).json({ msg: `No task with id : ${taskID}` });
+//         };
+//         res.status(200).json({ task });
+//         // res.status(200).send();
+//         // res.status(200).json({ task: null, status: 'success' });
+//     } catch (error) {
+//         res.status(500).json({ msg: error })
+//     };
+// };
+
+const deleteTask = asyncWrapper(async (req, res) => {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+        return res.status(404).json({ msg: `No task with id : ${taskID}` });
     };
-};
+    res.status(200).json({ task });
+});
 
 // Update Task (POST)
 
