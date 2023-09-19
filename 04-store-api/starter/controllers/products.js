6114549586adf41959ee3b8728e140b29e6c68bc
Brittany Halterman <halterma@gmail.com>
2023-09-18 20:22:40 -0400

    add comments

---
+++ 04-store-api/starter/controllers/products.js
@@ -0,0 +1,7 @@
+// The idea is that one can search by any or all of these attributes: featured, name, price, rating, company
+// For the numeric fields price and rating, one can also specify greater than, less than, or equal to
+// One can also specify a sort order 
+// Also one can specify a skip and a limit, to facilitate pagination through the result
+
+// That file has two methods, getAllProducts and getAllProductsStatic
+// The getAllProductsStatic method is just for experimentation
