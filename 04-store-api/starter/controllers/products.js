484c781fbb1f7a602c91453d9db003574b7e47fb
Brittany Halterman <halterma@gmail.com>
2023-09-24 21:23:38 -0400

    create general structure for filter functionality

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -7,20 +7,35 @@ const Product = require('../models/product');
 
 // Two methods, getAllProducts and getAllProductsStatic
 // The getAllProductsStatic method is just for experimentation
-const getAllProducts = async (req, res) => {
-    res.status(200).json({ msg: 'products route' });
-};
-
 const getAllProductsStatic = async (req, res) => {
     // throw new Error('testing async errors');
     const products = await Product.find({ 
         // featured: true,
-        name: 'vase table'
+        // name: 'vase table',
+        page: '2'
     })
     res.status(200)
     // .json({ msg: 'products testing route' })
     .json({ products, nbHits: products.length });
-}; 
+};
+
+const getAllProducts = async (req, res) => {
+    // console.log(req.query);
+    const { featured } = req.query;
+    const queryObject = {};
+
+    if (featured) {
+        queryObject.featured = featured === 'true'? true : false // ternary operator
+    }
+    console.log(queryObject);
+    // const products = await Product.find(req.query); 
+    // // access to query string params in req.query --> object passed into find()
+    const products = await Product.find(queryObject);
+    res.status(200).json({ 
+        // msg: 'products route'
+        products, nbHits: products.length 
+    });
+};
 
 module.exports = {
     getAllProducts,
