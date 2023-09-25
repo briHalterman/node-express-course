1fd1455eba31ade4ce109b3f839317e7bc27b554
Brittany Halterman <halterma@gmail.com>
2023-09-24 22:33:13 -0400

    implement mongoose sort functionality

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -15,8 +15,8 @@ const getAllProductsStatic = async (req, res) => {
         // name: 'vase table',
         // page: '2',
         // name: 'albany ', // 'albany sectional',
-        name: {$regex: search, $options: 'i'},
-    })
+        // name: {$regex: search, $options: 'i'},
+    }).sort('-name price')
     res.status(200)
     // .json({ msg: 'products testing route' })
     .json({ products, nbHits: products.length });
@@ -25,9 +25,10 @@ const getAllProductsStatic = async (req, res) => {
 // search based on featured status, company, name, price & rating
 const getAllProducts = async (req, res) => {
     // console.log(req.query);
-    const { featured, company, name } = req.query;
+    const { featured, company, name, sort } = req.query;
     const queryObject = {};
 
+    // FILTER
     if (featured) {
         queryObject.featured = featured === 'true'? true : false // ternary operator
     }
@@ -38,11 +39,26 @@ const getAllProducts = async (req, res) => {
         // queryObject.name = name;
         queryObject.name = { $regex: name, $options: 'i'}
     }
-    console.log(queryObject);
+    // console.log(queryObject);
 
     // const products = await Product.find(req.query); 
     // // access to query string params in req.query --> object passed into find()
-    const products = await Product.find(queryObject);
+    // const products = await Product.find(queryObject);
+    // const products = await Product.find(queryObject).sort();
+    // let products = await Product.find(queryObject); // need to remove await
+    let result = Product.find(queryObject);
+
+    // SORT
+    // sorting does not affect the number of items returned, just the order in which they are displayed
+    if (sort) {
+        // products = products.sort()
+        // console.log(sort);
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList);
+    } else {
+        result = result.sort('createdAt');
+    }
+    const products = await result;
     res.status(200).json({ 
         // msg: 'products route'
         products, nbHits: products.length 
