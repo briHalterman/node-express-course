014c494022ef96d7e855bcf434f38866ed02e98c
Brittany Halterman <halterma@gmail.com>
2023-09-25 17:26:18 -0400

    implement pagination functionality

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -19,8 +19,8 @@ const getAllProductsStatic = async (req, res) => {
     })
     .sort('name')
     .select('name price')
-    .limit(10)
-    .skip(5)
+    // .limit(10)
+    // .skip(5)
 
     res.status(200)
     // .json({ msg: 'products testing route' })
@@ -68,6 +68,16 @@ const getAllProducts = async (req, res) => {
         const fieldsList = fields.split(',').join(' ');
         result = result.select(fieldsList);
     }
+
+    // use skip and limit to set up a pagination functionality
+    const page = Number(req.query.page) || 1
+    const limit =   Number(req.query.limit) || 10
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
+    // 23 items by 7 items per page
+    // 4 pages: 7 items, 7 items, 7 items, 2 items
+
     const products = await result;
     res.status(200).json({ 
         // msg: 'products route'
