f23937be710ac40d349bf91052711a3c966eaa0e
Brittany Halterman <halterma@gmail.com>
2023-09-26 19:11:29 -0400

    review project setup

--- 05-JWT-Basics/starter/db/connect.js
+++ 05-JWT-Basics/starter/db/connect.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose')
+// Just so you don't forget the setup...
+const mongoose = require('mongoose');
 
 const connectDB = (url) => {
   return mongoose.connect(url, {
@@ -6,7 +7,7 @@ const connectDB = (url) => {
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-  })
-}
+  });
+};
 
-module.exports = connectDB
+module.exports = connectDB;
--- 05-JWT-Basics/starter/middleware/error-handler.js
+++ 05-JWT-Basics/starter/middleware/error-handler.js
@@ -1,9 +1,11 @@
-const CustomAPIError = require('../errors/custom-error')
+const CustomAPIError = require('../errors/custom-error'); // custom API error
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+  if (err instanceof CustomAPIError) { // look for that instance
+    return res.status(err.statusCode).json({ msg: err.message }) // if that is the case, err.StatusCode is the response
   }
-  return res.status(500).send('Something went wrong try again later')
-}
+  return res
+  .status(500) // otherwise generic 500 error response
+  .send('Something went wrong try again later');
+};
 
-module.exports = errorHandlerMiddleware
+module.exports = errorHandlerMiddleware;
