cee0680975d189b0fb4e7ea5125ee9cd1a15ccd1
Brittany Halterman <halterma@gmail.com>
2023-09-28 07:24:35 -0400

    set up postman

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -5,11 +5,14 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-    const luckyNumber = Math.floor(random()*100); // random number between 0 and 99
+    const luckyNumber = Math.floor(Math.random() * 100); // random number between 0 and 99
     res.status(200).json({ 
         msg: `Hello, John Doe`, 
         secret: `Here is your authorized data, your lucky number is ${luckyNumber}` 
     });
 };
 
-module.exports = { login, dashboard };
+module.exports = { 
+    login, 
+    dashboard 
+};
--- 05-JWT-Basics/starter/routes/main.js
+++ 05-JWT-Basics/starter/routes/main.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const { login, dashboard } = require('../controllers/main');
 
-router.route('./dashboard').get(dashboard); // /api/v1/dashboard
+router.route('/dashboard').get(dashboard); // /api/v1/dashboard
 router.route('/login').post(login); // /api/v1/login
 
 module.exports = router;
