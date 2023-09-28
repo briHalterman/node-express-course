0c8edcfef915c7bba97bdf8d5e6be7411a5fc85e
Brittany Halterman <halterma@gmail.com>
2023-09-28 10:43:18 -0400

    access username and password from req.body

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,6 +1,19 @@
 // controllers
 
+// if exist create new JWT
+// send back to front-end
+
+// set up authentication so only the request with JWT can access the dashboard
+
+const CustomAPIError = require('../errors/custom-error'); // import custom error
+
 const login = async (req, res) => {
+    const { username, password } = req.body;
+    // mongoose validations
+    // Joi
+    // check in the controller
+
+    console.log(username, password);
     res.send('Fake Login/Register/Signup'); // normally there will be a difference between logging in vs registering
 };
 
