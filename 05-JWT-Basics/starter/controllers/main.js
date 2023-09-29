174fd913711ada4a380d0d16fd061bd26a6917ad
Brittany Halterman <halterma@gmail.com>
2023-09-28 20:43:24 -0400

    set up JWT

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -5,9 +5,11 @@
 
 // set up authentication so only the request with JWT can access the dashboard
 
+const jwt = require('jsonwebtoken');
 const CustomAPIError = require('../errors/custom-error'); // import custom error
 
 // check for username, password in post(login) request
+// normally there will be a difference between logging in vs registering
 const login = async (req, res) => {
     const { username, password } = req.body;
     // mongoose validations
@@ -18,8 +20,17 @@ const login = async (req, res) => {
     if(!username || !password) {
         throw new CustomAPIError('Please provide email and password', 400);
     };
+    
+    // just for demo, normally provided by DB!!!!
+    const id = new Date().getDate()
+    
+    // create new token
+    // try to kepp payload small, better for user experience
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {expiresIn: '30d'}); // don't send confidential info!
+    
     // console.log(username, password);
-    res.send('Fake Login/Register/Signup'); // normally there will be a difference between logging in vs registering
+    // res.send('Fake Login/Register/Signup'); 
+    res.status(200).json({ msg: 'user created', token })    
 };
 
 const dashboard = async (req, res) => {
