45eb2de4f144d5c98895012247816893fe6dc3d8
Brittany Halterman <halterma@gmail.com>
2023-09-29 05:59:30 -0400

    set up errors index

--- 05-JWT-Basics/starter/errors/bad-request.js
+++ 05-JWT-Basics/starter/errors/bad-request.js
@@ -1,6 +1,6 @@
 const CustomAPIError = require('./custom-error')
 
-class BadRequest extends CustomAPIError { 
+class BadRequestError extends CustomAPIError { 
     constructor(message) {
       super(message)
       this.statusCode = 400
