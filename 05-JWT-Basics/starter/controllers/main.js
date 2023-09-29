1acbbf625f627986a1c4715b2bfa90d291a61445
Brittany Halterman <halterma@gmail.com>
2023-09-29 05:36:37 -0400

    refractor authentication to middleware

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -34,29 +34,31 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-    // console.log(req.headers);
-    const authHeader = req.headers.authorization;
-    
-    // very important that syntax is exact
-    if (!authHeader || !authHeader.startsWith('Bearer '))  {
-        throw new CustomAPIError('No token provided', 401) // "Invalid Credentials to Access This Route"
-    }
+    console.log(req.user);
 
-    const token = authHeader.split(' ')[1];
+    // console.log(req.headers);
+    // const authHeader = req.headers.authorization;
+    
+    // // very important that syntax is exact
+    // if (!authHeader || !authHeader.startsWith('Bearer '))  {
+    //     throw new CustomAPIError('No token provided', 401) // "Invalid Credentials to Access This Route"
+    // }
+
+    // const token = authHeader.split(' ')[1];
     // console.log(token);
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        // console.log(decoded);
-        const luckyNumber = Math.floor(Math.random() * 100);
-        res.status(200).json({ 
-            // msg: `Hello, John Doe`,
-            msg: `Hello, ${decoded.username}`, 
-            secret: `Here is your authorized data, your lucky number is ${luckyNumber}` 
-        });
-    } catch (error) {
-        throw new CustomAPIError('Not Authorized to Access This Route', 401) // 401 -- authentication error
-    };
+    // try {
+    //     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    //     // console.log(decoded);
+    //     const luckyNumber = Math.floor(Math.random() * 100);
+    //     res.status(200).json({ 
+    //         // msg: `Hello, John Doe`,
+    //         msg: `Hello, ${decoded.username}`, 
+    //         secret: `Here is your authorized data, your lucky number is ${luckyNumber}` 
+    //     });
+    // } catch (error) {
+    //     throw new CustomAPIError('Not Authorized to Access This Route', 401) // 401 -- authentication error
+    // };
 
     // move to try block
     // const luckyNumber = Math.floor(Math.random() * 100); // random number between 0 and 99
@@ -64,6 +66,13 @@ const dashboard = async (req, res) => {
     //     msg: `Hello, John Doe`, 
     //     secret: `Here is your authorized data, your lucky number is ${luckyNumber}` 
     // });
+
+    const luckyNumber = Math.floor(Math.random() * 100);
+    res.status(200).json({
+        // msg: `Hello, ${decoded.username}`, 
+        msg: `Hello, ${req.user.username}`,
+        secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+    });
 };
 
 module.exports = { 
--- 05-JWT-Basics/starter/middleware/auth.js
+++ 05-JWT-Basics/starter/middleware/auth.js
@@ -1,6 +1,28 @@
+const jwt = require('jsonwebtoken');
+const CustomAPIError = require('../errors/custom-error'); // check path
+
 const authenticationMiddleware = async (req, res, next) => {
-    console.log(req.headers.authorization);
-    next();
+    // console.log(req.headers.authorization);
+    const authHeader = req.headers.authorization;
+    
+    // check for authorization header
+    if (!authHeader || !authHeader.startsWith('Bearer '))  { // check for bearer
+        throw new CustomAPIError('No token provided', 401) 
+    }
+
+    const token = authHeader.split(' ')[1]; // split header
+
+    try {
+        // run verify method, pass in token and pass in secret
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const { id, username } = decoded; // from decoded
+        req.user = { id, username } // create new object
+        next();  // pass on to next middleware
+    } catch (error) {
+        throw new CustomAPIError('Not authorized to access this route', 401);
+    };
+
+    // next();
 };
 
 module.exports = authenticationMiddleware;
